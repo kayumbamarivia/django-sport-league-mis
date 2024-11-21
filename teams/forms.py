fbc6e795f33f0e3abd9889249b65b408f00420f3
jmv <kayumbaj88@gmail.com>
2024-11-21 07:19:57 +0200

    First version of my project is done

--- teams/forms.py
+++ teams/forms.py
@@ -1,4 +1,3 @@
-# forms.py
 from django import forms
 from .models import Team
 from users.models import User
@@ -6,11 +5,11 @@ from users.models import User
 STYLES =  'form-input border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500'
 class TeamForm(forms.ModelForm):
     players = forms.ModelMultipleChoiceField(
-        queryset=User.objects.filter(role='player'),
+        queryset=User.objects.none(), 
         required=False,
         widget=forms.SelectMultiple(attrs={
             'class': STYLES,
-            'id': 'id_players'  # Important for Select2 initialization
+            'id': 'id_players'
         })
     )
 
@@ -27,5 +26,18 @@ class TeamForm(forms.ModelForm):
             'class': STYLES
         })
         self.fields['players'].widget.attrs.update({
-            'style': 'width: 100%;' 
+            'style': 'width: 100%;'
         })
+        current_team = kwargs.get('instance', None)
+        if current_team:
+            self.fields['coach'].queryset = User.objects.filter(role='coach').exclude(
+                coached_teams__isnull=False
+            ).union(User.objects.filter(coached_teams=current_team))
+        else:
+            self.fields['coach'].queryset = User.objects.filter(role='coach', coached_teams__isnull=True)
+        if current_team:
+            self.fields['players'].queryset = User.objects.filter(role='player').exclude(
+                player_teams__isnull=False
+            ).union(current_team.players.all())
+        else:
+            self.fields['players'].queryset = User.objects.filter(role='player', player_teams__isnull=True)
